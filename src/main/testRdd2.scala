58b5cd102b8193501f38c280d62c457a157588f6
hukexin <hukexin@xiaomi.com>
2018-06-22 21:36:06 +0800

    init

---
+++ src/main/testRdd2.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
+
+object testRdd2 {
+    def main(args: Array[String]) {
+        val conf = new SparkConf().setAppName("SingleRDD").setMaster("local[8]")
+        val sc = new SparkContext(conf)
+        // 设置日志等级
+        sc.setLogLevel("WARN")
+        // jsonFile.txt内容如上图
+        val data: RDD[String] = sc.textFile("jsonFile.txt")
+        // SingleRDD 去重=>distinct API
+        data.distinct().foreach(println)
+        // 结果
+        //{"name":"李四","age":26,"from":"HuNan"}
+        //{"name":"王五","age":26,"from":"HuBei"}
+        //{"name":"张三","age":24,"from":"HuBei"}
+    }
+}
---
+++ src/test/testRdd2.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.FunSuite
+
+class testRdd2 extends FunSuite{
+    test("test2") {
+        println("unit test, hello world!")
+    }
+}
