36b77a56e97aac91d1201b5bf59d36c4c4a0073b
hukexin <hukexin@xiaomi.com>
2018-10-25 13:11:36 +0800

    update

--- src/main/scala/testObject.scala
+++ src/main/scala/testObject.scala
@@ -1,8 +1,12 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class SearchDocResult(docId:String, docName:String, query:String)
+
 object testObject {
     def main(args: Array[String]) {
         println(s"class name:${getClass.toGenericString}")
+        val result = SearchDocResult("1", "test", "hello")
+        println(result.productIterator.mkString(" "))
     }
 }
