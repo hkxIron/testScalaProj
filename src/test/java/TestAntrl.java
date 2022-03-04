b40fff2fc2459ad8d3f5038633ac9a14f3e6346c
hukexin <hukexin@xiaomi.com>
2022-03-04 19:39:15 +0800

    add

---
+++ src/test/java/TestAntrl.java
@@ -0,0 +1,18 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author hukexin
+ * @date 20-12-30
+ */
+public class TestAntrl {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+    }
+
+    @Test
+    public void antrl() throws Exception {
+        System.out.println("hello, antrl");
+    }
+}
